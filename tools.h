a6ed68299c89ecd067f341c783dd71c565cdbdc8
SikeC <1263686918@qq.com>
2017-09-27 23:13:08 +0800

    Create tools.h

---
+++ tools.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef TOOLS_H
+#define TOOLS_H
+
+/*摘录至http://www.cnblogs.com/sosoft/p/3508072.html*/
+
+void SetWindowSize(int cols, int lines);
+void SetCursorPosition(const int x, const int y);
+void SetColor(int colorID);
+void SetBackColor();
+
+#endif // TOOLS_H
